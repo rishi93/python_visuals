b6109c3df5604cbb8ad846c31f416a1c35083a7e
Rishi <rishi93dev@gmail.com>
2019-05-07 16:03:39 +0200

    first commit

---
+++ sawtooth.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t = np.linspace(0, 2, 1000)
+
+y = 1/2
+
+sines = [-1/(2*n*np.pi) * np.sin(2*np.pi*n*t) for n in range(1, 50, 1)]
+
+for sine in sines:
+    y = y + sine
+
+plt.figure()
+plt.title('Fourier series of sawtooth wave')
+plt.plot(t, y)
+plt.show()
---
+++ square.py
@@ -0,0 +1,16 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+t = np.linspace(0, 2, 1000)
+
+y = 1/2
+
+sines = [2/(np.pi*n) * np.sin(n*2*np.pi*t) for n in range(1, 50, 2)]
+
+for sine in sines:
+    y = y + sine
+
+plt.figure()
+plt.title('Fourier series of square wave')
+plt.plot(t, y)
+plt.show()
